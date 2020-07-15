10aae740f2fd6b072e00ab85a84db77d41ae4be3
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-15 23:25:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,6 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   final_results = data.each_with_object({}) do |(key, value), final_array|
-    value.each 
+    value.each do |inner_key, names|
+      
 end
